103405a1b6a1db472d912c9d3c34f3b9aaa77286
Samuel Murcio <sam.murcio@gmail.com>
2016-11-30 12:18:30 -0600

    V1.1

--- Get-Alpha2Code.ps1
+++ Get-Alpha2Code.ps1
@@ -1,26 +1,29 @@
-﻿param(
+param(
+    [Parameter(Mandatory=$true)
     [string]$csv
   )
 
 $list = Import-Csv $csv
+$outFile = = $env:USERPROFILE + "\Desktop\Countries.csv"
 
 foreach ( $a in $list ) {
-  $searchStr = $a.Name.Replace(' ','%20')
+  $searchStr = $a.Country.Trim()
+  $searchStr = $searchStr.Replace(' ','%20')
 
   try {
-    $result = Invoke-RestMethod -Uri "https://restcountries.eu/rest/v1/name/$($a.Name)?fullText=true)" -ErrorAction Stop
+    $result = Invoke-RestMethod -Uri "https://restcountries.eu/rest/v1/name/$($searchStr)?fullText=true)" -ErrorAction Stop
     $result = $result.alpha2Code
   } catch {
     $result = "Unable to find country"
   }
 
   $obj = [pscustomobject]@{
-    Name = $a.Name
+    Name = $a.Country
     Alpha2 = $result
     }
 
-  $obj | Export-Csv -Path $csv -NoTypeInformation -Append
+  $obj | Export-Csv -Path $outFile -NoTypeInformation -Append
 }
 
-Write-Output "Results log saved here: $csv"
+Write-Output "Results log saved here: $outFile"
 
