14f1f4cb3453df3efee17ae0b63af80649ce6a93
Samuel Murcio <sam.murcio@gmail.com>
2016-11-30 12:02:25 -0600

    V1.0

---
+++ Get-Alpha2Code.ps1
@@ -0,0 +1,26 @@
+﻿param(
+    [string]$csv
+  )
+
+$list = Import-Csv $csv
+
+foreach ( $a in $list ) {
+  $searchStr = $a.Name.Replace(' ','%20')
+
+  try {
+    $result = Invoke-RestMethod -Uri "https://restcountries.eu/rest/v1/name/$($a.Name)?fullText=true)" -ErrorAction Stop
+    $result = $result.alpha2Code
+  } catch {
+    $result = "Unable to find country"
+  }
+
+  $obj = [pscustomobject]@{
+    Name = $a.Name
+    Alpha2 = $result
+    }
+
+  $obj | Export-Csv -Path $csv -NoTypeInformation -Append
+}
+
+Write-Output "Results log saved here: $csv"
+
